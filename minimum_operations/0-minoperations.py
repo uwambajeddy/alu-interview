5f0ccff2bcc2ec19d72bbea8b985684bcb32ae81
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2025-01-26 20:34:11 +0200

    rain

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-Minimum operations to reach n characters
-"""
+""" Minimum Operations"""
 
 
 def minOperations(n):
-    """
-    Calculates the minimum operations to reach n characters
-
-    Args:
-        n (int): The number of characters to reach
-
-    Returns:
-        int: The minimum operations to reach n characters
-    """
-    # Edge cases [0, 1]
+    """ Minimum Operations"""
     if n <= 1:
         return 0
-
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        if n % divisor == 0:
-            n = n // divisor
-            operations += divisor
+    i = 2
+    result = 0
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n = n / i
         else:
-            divisor += 1
-    return operations
+            i += 1
+    return result
